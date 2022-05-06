14e1eafdae593145d358c3a2b108fcc0f1f32c21
Ryan <102423466+50t0r25@users.noreply.github.com>
2022-05-06 05:04:29 +0100

    Few additions: Added google services dependencies Added Firebase SDK dependencies Threw together some quick test code for the DB

--- app/src/main/java/dz/notacompany/el_cous/HomeFragment.kt
+++ app/src/main/java/dz/notacompany/el_cous/HomeFragment.kt
@@ -2,14 +2,48 @@ package dz.notacompany.el_cous
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private val db = Firebase.firestore
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        var departureList = arrayListOf<String>()
+        var destinationList = arrayListOf<String>()
+
+        db.collection("trajets").get()
+            .addOnSuccessListener { trajets ->
+                for ((i, trajet) in trajets.withIndex()) {
+                    departureList.add(trajet.data["depart"].toString())
+                    destinationList.add(trajet.data["destination"].toString())
+                }
+                departureSpinner.setItems(departureList)
+            }
+
+
+        destinationSpinner.setItems(destinationList)
+        destinationSpinner.isClickable = false
+        departureSpinnerLayout.setOnClickListener {
+            if (!destinationSpinner.isClickable) {
+                Toast.makeText(context, "Choissisez un depart!", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        departureSpinner.setOnItemSelectedListener { view, position, id, item ->
+            destinationSpinner.isClickable = true
+            Toast.makeText(context, departureList[position], Toast.LENGTH_LONG).show()
+        }
+
+        destinationSpinner.setOnItemSelectedListener { view, position, id, item ->
+            Toast.makeText(context, destinationList[position], Toast.LENGTH_LONG).show()
+        }
 
     }
 }
